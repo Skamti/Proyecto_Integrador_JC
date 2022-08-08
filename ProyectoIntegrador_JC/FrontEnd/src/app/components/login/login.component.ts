bfdff977e57a2b244995736447040c586638ca3a
Skamti <95065443+Skamti@users.noreply.github.com>
2022-08-08 01:57:52 -0300

    cambiado css y media query para foto perf
    arreglado  error que muestre contraseña o usuario equivocado

--- ProyectoIntegrador_JC/FrontEnd/src/app/components/login/login.component.ts
+++ ProyectoIntegrador_JC/FrontEnd/src/app/components/login/login.component.ts
@@ -44,7 +44,7 @@ export class LoginComponent implements OnInit {
         this.isLogginFail = true;
         this.errMsj = err.error.mensaje;
         console.log(this.errMsj);
-        
+        alert("Un campo ingresado fue erroneo, por favor vuelva a ingresar usuario y contraseña");        
       })
   }
 
